615b7595c8afd54c520889fc438e1959f724f356
Sacha WILT <sacha.wilt@gmail.com>
2018-04-13 16:20:27 +0200

    misc: removed dotenv

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
 import axios from 'axios';
-import dotenv from 'dotenv';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
 import './assets/sass/main.scss';
-
-dotenv.config();
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
